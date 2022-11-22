cb75c42582222eb3b7537b6acc0a80959eda6e80
Oren Izmirli <oreni@bezeqint.co.il>
2022-11-22 19:40:18 +0200

    Exercise#7 WIP

---
+++ 2022B3/ex07Socket_file_functions/solution_server.py
@@ -0,0 +1,94 @@
+"""Dynamic function server (Pickled).
+
+Listen at fixed port (9999);
+Support functions from "server_func*.py" local files, and 'bye" command.
+Respond in "pickle" format.
+Logging to console.
+"""
+import glob
+import logging
+import pickle
+import socket
+
+SERVER_IP = '127.0.0.1'  # localhost IP.
+SERVER_PORT = 9999
+
+logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
+
+
+def get_actions_from_local_files() -> dict:
+    """Return actions functions-dict for server.
+
+    Find local files with names of this pattern: "server_func_*.py"
+    Load their functions to the "actions" mapping.
+    Add 2 special functions: "bye" and "" (empty string for unknown).
+
+    :return: mapping for functions the server supports.
+    """
+    actions = {
+        'bye': lambda x: "bye",
+        '': lambda x: f"Unknown command '{x}'",
+    }
+    for func_file in glob.iglob('server_func_*.py'):
+        with open(func_file) as ffh:
+            code = ffh.read()
+        exec(code, actions)
+
+    return actions
+
+
+def run_server():
+    """Runs a single connection server, respond in pickle format.
+
+    Support pre-defined message types, otherwise sends "Unknown" respond.
+
+    :return: None
+    """
+    actions = get_actions_from_local_files()
+
+    sock: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP/IP
+    sock.bind((SERVER_IP, SERVER_PORT))  # bind server's address.
+    sock.listen()
+    logging.info("Server is UP (listening at %s:%d)" % (SERVER_IP, SERVER_PORT))
+
+    client_sock = get_client_connection(sock)
+    while True:
+        try:
+            data = client_sock.recv(1024).decode().strip()  # receive up to 1KB bytes & decode to str.
+        except ConnectionAbortedError as cae:
+            logging.error(cae)
+            break
+        command = data.split()[0] if data else ''
+        if not command:  # looks like user has disconnected.
+            logging.warning("User has disconnected before 'bye' message.")
+            client_sock.close()
+            break
+
+        if command not in actions:
+            logging.warning(f"Unknown command '{data}'")
+            command = ''
+
+        respond = actions[command](data[len(command):].strip())
+        sent = client_sock.send(pickle.dumps(respond))
+        logging.info("Sent %d bytes in respond to '%s' message." % (sent, command if command else 'Unknown'))
+        if command == 'bye':
+            client_sock.close()
+            break
+
+    sock.close()
+    logging.info("Server closed")
+
+
+def get_client_connection(sock: socket) -> socket:
+    """Accept connection from client and return the socket to it.
+
+    :param sock: server socket
+    :return: client socket
+    """
+    (client_sock, client_address) = sock.accept()
+    logging.info("Client connected from: %s:%d" % client_address)
+    return client_sock
+
+
+if __name__ == '__main__':
+    run_server()
---
+++ 2022B3/ex07Socket_file_functions/test_solution.py
@@ -0,0 +1,41 @@
+import pytest
+from threading import Thread
+
+from solution_server import run_server
+from solution_client import get_client_socket, send_client_message
+
+
+@pytest.fixture
+def running_server():
+    t = Thread(target=run_server)
+    t.start()
+    yield
+
+
+@pytest.fixture()
+def client_socket():
+    s = get_client_socket()
+    yield s
+    s.close()
+
+
+def test_numbers(running_server, client_socket):
+    assert send_client_message(client_socket, 'numbers') == list(range(10))
+
+
+def test_reverse_word(running_server, client_socket):
+    assert send_client_message(
+        client_socket, 'reverse_word elephant') == 'tnahpele'
+
+
+def test_unicode_map(running_server, client_socket):
+    assert send_client_message(client_socket, 'unicode_map abracadabra') == {
+        'a': 97, 'b': 98, 'c': 99, 'd': 100, 'r': 114}
+
+
+def test_bad_command(running_server, client_socket):
+    assert send_client_message(client_socket, 'blah blah blah').strip() == "Unknown command 'blah blah blah'"
+
+
+def test_saying_goodbye(running_server, client_socket):
+    assert send_client_message(client_socket, 'bye').strip() == 'bye'
